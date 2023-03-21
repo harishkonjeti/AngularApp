059f48c0239e144d19056655ac7bdfc11f08d575
Hareesh Konjeti <harishkonjeti34@gmail.com>
2023-03-21 23:04:52 +0530

    Implemented ngOnInit

--- src/app/modules/shared/layout/footer/footer.component.ts
+++ src/app/modules/shared/layout/footer/footer.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-
+  
+  title : string | undefined;
   constructor() { }
 
   ngOnInit(): void {
+    console.log("footer : ngOnInIt");
+    //initialises in ngOnIt instead of constructor
+    this.title = "This is a footer";
   }
 
 }
--- src/app/modules/shared/layout/header/header.component.ts
+++ src/app/modules/shared/layout/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
     selector:'app-header',
@@ -6,7 +6,11 @@ import { Component } from "@angular/core";
     styleUrls:['./header.component.scss']
 })
 
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+    
+    ngOnInit(): void {
+        // throw new Error("Method not implemented.");
+    }
     // constructor(parameters) {
         
     // }
