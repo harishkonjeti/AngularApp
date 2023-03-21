f885026711da76244e1659add90028fca190bd63
Hareesh Konjeti <harishkonjeti34@gmail.com>
2023-03-21 21:43:57 +0530

    Created Header and footer for angular app

---
+++ src/app/modules/shared/shared.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from "@angular/core";
+import { HeaderComponent } from "./layout/header/header.component";
+import { FooterComponent } from './layout/footer/footer.component';
+
+@NgModule({
+    declarations :[
+        HeaderComponent,
+        FooterComponent
+    ],
+    exports : [
+        HeaderComponent,
+        FooterComponent
+    ]
+})
+
+export class SharedModule{}
